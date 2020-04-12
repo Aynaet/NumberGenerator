9dbda7880c208b765977066226fa47f101a9c97e
Aynaet <63262664+Aynaet@users.noreply.github.com>
2020-04-12 22:42:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
- os.system("clear")
+try:
+
+    W = '\033[0m'  #White
+    R = '\033[31m' #Red
+    G = '\033[32m' #Green
   
- 
+    
+    os.system("clear")
+  
+    
+    
+    print("             WhatsappApks | Aynaet-Git              ")
 
-print("               WhatsappApks | Aynaet:Git              ")
+    server = raw_input ('Did you subribed to Aynaet: ')
 
-const confirm = await this.confirm ('Do you subscribed to me?')
-console.log (confirm)
+    if server == 'yes' or server == 'Yes' :
+       
+    print(" All links:")
+    
+        
+    else:
+
+        print(R + "Error - This script only works for subscribers." + W)
+        sys.exit()
+
+   
