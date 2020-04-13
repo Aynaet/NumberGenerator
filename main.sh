e7a9a963641eba8379226a4773209a517ff463cd
Aynaet <63262664+Aynaet@users.noreply.github.com>
2020-04-13 14:15:56 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,7 +20,7 @@ clear
  echo $mer" Delta Whatsapp https://bit.ly/2xt5Epd"
  echo $mer" DM Whatsapp https://bit.ly/3a27lab"
  echo $mer" Dioses Whatsapp https://bit.ly/2Rvuo6P"
- echo $mer" K.P.H Whatsapp https://bit.ly/2xt5Epd"
+ echo $mer" K.P.H Whatsapp https://bit.ly/3ekJK83"
  echo $mer" OB Whatsapp https://bit.ly/2Xwcftr"
  echo $mer" HAWA Whatsapp https://bit.ly/2Xw75hi"
  echo $mer" Sudani Whatsapp https://bit.ly/2VrwA0i"
