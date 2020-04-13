833c1370e97a3790ea29b9197d53be3c3e71ab14
Aynaet <63262664+Aynaet@users.noreply.github.com>
2020-04-13 12:14:37 +0200

    Update and rename main.py to main.sh

---
+++ main.sh
@@ -0,0 +1,27 @@
+
+
+#Mod by        : Aynaet
+#Tool Unfaedah : Hehe;"v"
+
+clear
+
+                                          
+
+ echo $mer"               WhatsappApks | Aynaet:Git              "
+
+
+ echo ""
+ echo -n $cy" Subscribed to Aynaet?: "
+ read t
+ echo $pur"<•===========================•>"
+ echo $mer" Here are all the links:"
+ echo $mer" GB Whatsapp https://bit.ly/3b5k0dQ"
+ echo $mer" GB Whatsapp https://bit.ly/3b5k0dQ"
+ echo $mer" GB Whatsapp https://bit.ly/3b5k0dQ"
+
+
+
+
+ #!/bin/bash
+
+   
